57db9d1435eb4d6141c59a960f4fc68442fca86c
PuffyJey <elrugido974@gmail.com>
2026-04-18 20:56:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,34 @@ function startBot() {
   });
 
   bot.on('spawn', () => {
+    console.log('Conectado al server');
+
+    setInterval(() => {
+      bot.setControlState('jump', true);
+      setTimeout(() => bot.setControlState('jump', false), 500);
+    }, 30000);
+  });
+
+  bot.on('end', () => {
+    console.log('Desconectado, reintentando...');
+    setTimeout(startBot, 10000);
+  });
+
+  bot.on('error', (err) => {
+    console.log('Error:', err.message);
+  });
+}
+
+startBot();const mineflayer = require('mineflayer');
+
+function startBot() {
+  const bot = mineflayer.createBot({
+    host: 'gentlesmp.aternos.me',
+    port: 13163,
+    username: 'AFK_Bot'
+  });
+
+  bot.on('spawn', () => {
     console.log('Conectado');
 
     setInterval(() => {
